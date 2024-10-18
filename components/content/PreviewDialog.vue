70ad9e3ead49664407577a5b499274c656c7309b
Johann Schopplich <mail@johannschopplich.com>
2024-10-18 16:15:02 +0200

    docs(translator): add `bulk` translation feature guide

---
+++ components/content/PreviewDialog.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  src: string;
+}>();
+</script>
+
+<template>
+  <div class="inline-block rounded-md bg-[#F0F0F0]">
+    <NuxtImg
+      :src="src"
+      alt="Kirby Tools section preview"
+      class="!my-0 w-[22rem] rounded-md"
+    />
+  </div>
+</template>
