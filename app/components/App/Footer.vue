eb53bcf56f32e71414b1ee4dd2de1f88494ea08c
Johann Schopplich <mail@johannschopplich.com>
2025-06-26 16:30:28 +0200

    chore: fix formatting

--- app/components/App/Footer.vue
+++ app/components/App/Footer.vue
@@ -81,13 +81,11 @@ const columns: FooterColumn[] = [
         <UFooterColumns :columns="columns">
           <template #right>
             <h3 class="text-sm font-semibold">About</h3>
-            <p
-              class="mt-6 text-sm"
-            >
+            <p class="mt-6 text-sm">
               Kirby Tools is not affiliated with the developers of Kirby CMS.
               Having said that, we love Kirby. Our plugins wouldn't exist
               without it. 🧡
-          </p>
+            </p>
           </template>
         </UFooterColumns>
       </UContainer>
