2eb118b4baa241dfb4bd9ae582bec1a639ea75ef
Johann Schopplich <mail@johannschopplich.com>
2025-07-02 14:33:13 +0200

    fix: streamline pricing redirect logic and update nuxt config for old links

--- app/middleware/pricing-redirect.ts
+++ app/middleware/pricing-redirect.ts
@@ -1,13 +1,13 @@
 import { withoutTrailingSlash } from "ufo";
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (import.meta.client && to.hash === "#pricing") {
+  if (to.hash === "#pricing") {
     if (withoutTrailingSlash(to.path) === "/live-preview") {
-      window.location.href = "/live-preview/buy/";
+      return navigateTo("/live-preview/buy/");
     }
 
     if (withoutTrailingSlash(to.path) === "/content-translator") {
-      window.location.href = "/content-translator/buy/";
+      return navigateTo("/content-translator/buy/");
     }
   }
 });
