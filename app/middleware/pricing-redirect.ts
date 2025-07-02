cc918d3d9f1c5e1b2be5986b0efbb081ca56d83b
Johann Schopplich <mail@johannschopplich.com>
2025-07-02 11:55:56 +0200

    fix: add trailing slashes to redirect paths in pricing middleware

--- app/middleware/pricing-redirect.ts
+++ app/middleware/pricing-redirect.ts
@@ -3,11 +3,11 @@ import { withoutTrailingSlash } from "ufo";
 export default defineNuxtRouteMiddleware((to) => {
   if (import.meta.client && to.hash === "#pricing") {
     if (withoutTrailingSlash(to.path) === "/live-preview") {
-      return navigateTo("/live-preview/buy", { redirectCode: 301 });
+      return navigateTo("/live-preview/buy/", { redirectCode: 301 });
     }
 
     if (withoutTrailingSlash(to.path) === "/content-translator") {
-      return navigateTo("/content-translator/buy", { redirectCode: 301 });
+      return navigateTo("/content-translator/buy/", { redirectCode: 301 });
     }
   }
 });
