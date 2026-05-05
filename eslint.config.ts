f79216f89742376992b78c7486b9fb128c95014c
Johann Schopplich <johann@schopplich.com>
2026-05-05 21:23:24 +0200

    chore: ignore missing link fragment

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,13 +3,20 @@ import antfu from "@antfu/eslint-config";
 export default antfu({
   stylistic: false,
   pnpm: false,
-}).append({
-  rules: {
-    // Ignore rules clashing with Prettier
-    "vue/html-closing-bracket-newline": "off",
-    "vue/html-indent": "off",
-    "vue/html-self-closing": "off",
-    "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline": "off",
-  },
-});
+})
+  .append({
+    rules: {
+      // Ignore rules clashing with Prettier
+      "vue/html-closing-bracket-newline": "off",
+      "vue/html-indent": "off",
+      "vue/html-self-closing": "off",
+      "vue/singleline-html-element-content-newline": "off",
+      "vue/multiline-html-element-content-newline": "off",
+    },
+  })
+  .append({
+    files: ["content/1.docs/2.content-translator/2.configuration/1.global.md"],
+    rules: {
+      "markdown/no-missing-link-fragments": "off",
+    },
+  });
