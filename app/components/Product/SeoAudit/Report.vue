7d918b069c815d48930e9e2b7bbd87ec9b1ff28e
Johann Schopplich <johann@schopplich.com>
2026-09-16 15:43:57 +0200

    fix(seo-audit): show no bad results in the report mock

--- app/components/Product/SeoAudit/Report.vue
+++ app/components/Product/SeoAudit/Report.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const RATINGS: PanelSeoAuditRatings = {
-  seo: { rating: "bad" },
+  seo: { rating: "ok" },
   readability: { rating: "good" },
 };
 
@@ -17,10 +17,6 @@ const RESULTS: PanelSeoAuditResults = {
     {
       rating: "ok",
       text: '<a href="https://yoa.st/33v">Keyphrase density</a>: The keyphrase was found 3 times. That\'s less than the recommended minimum of 5 times for a text of this length. <a href="https://yoa.st/33w">Focus on your keyphrase</a>!',
-    },
-    {
-      rating: "bad",
-      text: '<a href="https://yoa.st/34d">Meta description length</a>: No meta description has been specified. Search engines will display copy from the page instead. <a href="https://yoa.st/34e">Make sure to write one</a>!',
     },
   ],
   readability: [
